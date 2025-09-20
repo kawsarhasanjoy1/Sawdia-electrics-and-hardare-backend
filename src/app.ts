86a222e401021bec8169823396358295570fb63c
Md Kawsar <kawsarhasanjoy342@gmail.com>
2025-09-20 09:42:00 -1200

    update

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,15 @@ import { secureApi } from "./app/middleware/secureApi";
 export const app = express();
 
 app.use(cookieParser());
-app.use(cors({ origin: ["https://sawdia-electrics-and-hardare-frontend-1.onrender.com"], credentials: true }));
+app.use(
+  cors({
+    origin: [
+      "https://sawdia-electrics-and-hardare-frontend-1.onrender.com",
+      "http://localhost:3000",
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
--- src/app/modules/auth/controller.ts
+++ src/app/modules/auth/controller.ts
@@ -3,76 +3,74 @@ import { catchAsync } from "../../utils/catchAsync";
 import { AuthServices } from "./services";
 import sendResponse from "../../utils/sendResponse";
 
-
 const loginUser = catchAsync(async (req, res) => {
-    const result = await AuthServices.loginUser(req.body);
-    const { refreshToken, accessToken } = result;
+  const result = await AuthServices.loginUser(req.body);
+  const { refreshToken, accessToken } = result;
 
-    res.cookie('refreshToken', refreshToken, {
-        httpOnly: true,
-        secure: false,
-        sameSite: 'lax',
-        maxAge: 1000 * 60 * 60 * 24 * 7,
-        path: '/',
-    });
+  res.cookie("refreshToken", refreshToken, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+    path: "/",
+  });
 
-    sendResponse(res, {
-        statusCode: StatusCodes.OK,
-        success: true,
-        message: 'User is logged in successfully!',
-        data: {
-            accessToken,
-        },
-    });
+  sendResponse(res, {
+    statusCode: StatusCodes.OK,
+    success: true,
+    message: "User is logged in successfully!",
+    data: {
+      accessToken,
+    },
+  });
 });
 
-
 const changePassword = catchAsync(async (req, res) => {
-    const result = await AuthServices.changePassword(req.user, req.body);
-    sendResponse(res, {
-        statusCode: StatusCodes.OK,
-        success: true,
-        message: 'Password changed successfully!',
-        data: result,
-    });
+  const result = await AuthServices.changePassword(req.user, req.body);
+  sendResponse(res, {
+    statusCode: StatusCodes.OK,
+    success: true,
+    message: "Password changed successfully!",
+    data: result,
+  });
 });
 
 const createAccessToken = catchAsync(async (req, res) => {
-    const result = await AuthServices.createAccessToken(req.cookies.refreshToken);
+  const result = await AuthServices.createAccessToken(req.cookies.refreshToken);
 
-    sendResponse(res, {
-        statusCode: StatusCodes.OK,
-        success: true,
-        message: 'Token created successfully!',
-        data: result,
-    });
+  sendResponse(res, {
+    statusCode: StatusCodes.OK,
+    success: true,
+    message: "Token created successfully!",
+    data: result,
+  });
 });
 
 const forgetPassword = catchAsync(async (req, res) => {
-    const result = await AuthServices.forgetPassword(req.body.email);
-    sendResponse(res, {
-        statusCode: StatusCodes.OK,
-        success: true,
-        message: 'Reset password link sent successfully!',
-        data: result,
-    });
+  const result = await AuthServices.forgetPassword(req.body.email);
+  sendResponse(res, {
+    statusCode: StatusCodes.OK,
+    success: true,
+    message: "Reset password link sent successfully!",
+    data: result,
+  });
 });
 
 const resetPassword = catchAsync(async (req, res) => {
-    const token = req.headers.authorization;
-    const result = await AuthServices.resetPassword(req.body, token as string);
-    sendResponse(res, {
-        statusCode: StatusCodes.OK,
-        success: true,
-        message: 'Password reset successfully!',
-        data: result,
-    });
+  const token = req.headers.authorization;
+  const result = await AuthServices.resetPassword(req.body, token as string);
+  sendResponse(res, {
+    statusCode: StatusCodes.OK,
+    success: true,
+    message: "Password reset successfully!",
+    data: result,
+  });
 });
 
 export const AuthControllers = {
-    loginUser,
-    changePassword,
-    createAccessToken,
-    forgetPassword,
-    resetPassword
+  loginUser,
+  changePassword,
+  createAccessToken,
+  forgetPassword,
+  resetPassword,
 };
