32e3db92bd876435b5942a69a2b5ad96d660f897
Md Kawsar <kawsarhasanjoy342@gmail.com>
2025-09-19 12:56:37 -1200

    updates

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ import { secureApi } from "./app/middleware/secureApi";
 export const app = express();
 
 app.use(cookieParser());
-app.use(cors({ origin: ["http://localhost:3000"], credentials: true }));
+app.use(cors({ origin: ["https://sawdia-electrics-and-hardare-frontend.onrender.com"], credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
